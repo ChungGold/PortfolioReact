4bf2b035cc3257371b07b481ad06b24a12215150
chunggold <chungau@csu.fullerton.edu>
2021-04-10 22:40:53 -0700

    added in reverse typing effect

--- src/components/Typer.jsx
+++ src/components/Typer.jsx
@@ -1,20 +1,44 @@
 import {useState, useEffect, useRef} from 'react';
 
-const words = 'Hello, my name is Chung Au';
+const words = [
+    'Hello, my name is Chung Au', 
+    `I'm a front-end developer`, 
+    'I made this typewriter component in React', 
+    `Although.. I'm still trying to figure out how to slot in a cleanup function`,
+    `Seriously, if I don't keep adding more phrases in the array, my portfolio will crash`];
 
 const Typer = props => {
     const [msg, setMsg] = useState(0);
     const [blink, setBlink] = useState('|');
     
     //Typing effect
-    const countRef = useRef(1);
+    const countRef = useRef(0);
+    const seqeuence = useRef(false);
+    const arrPos = useRef(0);
 
     useEffect(() => {
-        const timeout = setTimeout(() => {
-            setMsg(words.substring(0, countRef.current));
-            countRef.current++;
-        }, 75);
-        return () => clearTimeout(timeout);
+        if (seqeuence.current === false) {
+            const timeout = setTimeout(() => {
+                setMsg(words[arrPos.current].substring(0, countRef.current));
+                countRef.current++;
+
+                if (countRef.current === words[arrPos.current].length) {
+                    clearInterval(timeout);
+                    seqeuence.current = true;
+                }
+            }, 75);
+        } else if (seqeuence.current === true) {
+            const timeout2 = setTimeout(() => {
+                setMsg(words[arrPos.current].substring(0, countRef.current));
+                countRef.current--;
+
+                if (countRef.current === 0) {
+                    clearTimeout(timeout2);
+                    seqeuence.current = false;
+                    arrPos.current++;
+                }
+            }, 75);
+        }
     }, [msg]);
 
     //Blinker
