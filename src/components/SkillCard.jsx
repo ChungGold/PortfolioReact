f8ad1e543bc0b4d34970c5bf1f21ba21c83194f6
chunggold <chungau@csu.fullerton.edu>
2021-05-13 17:57:52 -0700

    redone to deploy properly

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,11 +1,11 @@
-import {SiHtml5, SiCss3, SiJavascript, SiReact} from 'react-icons/si';
+// import {SiHtml5, SiCss3, SiJavascript, SiReact} from 'react-icons/si';
 // , SiBootstrap, SiSass, SiTailwindcss, SiJquery
 
 const SkillCard = props => {
     return (<figure className = 'skill'>
-        <figure className = 'skillSvg'>
+        {/* <figure className = 'skillSvg'>
             <SiHtml5 />
-        </figure>
+        </figure> */}
         <figcaption className = 'skill-name'>
             <h1>{props.name}</h1>
         </figcaption>
